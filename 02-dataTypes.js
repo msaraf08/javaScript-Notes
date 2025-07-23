66041038225ad78471e9961f45ac722470c591b7
msaraf08 <manish.saraf1994@gmail.com>
2025-07-23 16:54:07 +0530

    initialize data type notes

---
+++ 02-dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer Js version.
+
+let name = "Manish"
+let age = 30
+let isLogedIn = false
+
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// Symbol => unique
+
+// object
+
+console.log(typeof "Manish");
+console.log(typeof age);
+
